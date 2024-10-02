efd6cab709d2a2ecab32b18c48f98e8fffbe4bc7
jk-20244 <jk08250825@gmail.com>
2024-10-02 17:18:07 +0100

    footer

--- app/component/footer.tsx
+++ app/component/footer.tsx
@@ -1,8 +1,19 @@
-export default function Footer(){
-    return(
-<div className="">
-
-     
-</div>
-    );
+export default function Footer() {
+  return (
+    <div className="flex w-full bottom-0 bg-black">
+      <div className="text-white">
+        +243 997 679 336 congoliciousfoundation@gmail.com
+      </div>
+      <div className="text-white">
+        Congolicious Foundation est une association sans but lucrative régulée
+        par les autorités congolaises. Enregistrée au ministère des affaires
+        sociales, actions humanitaires et solidarité nationale sous le numéro
+        280/0.E/2022 et au ministère du plan et suivi de la mise en œuvre pour
+        la révolution de la modernité sous le numéro 2044/PL/DCRE/22
+      </div>
+      <div className="text-white">
+        © 2023 Congolicious Foundation. Tous les droits sont réservés
+      </div>
+    </div>
+  );
 }
